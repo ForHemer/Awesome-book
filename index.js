e084aeec0a7936386349dbd07ca62febd14faeea
ForHemer <foreste.hemerson@gmail.com>
2022-02-08 08:32:11 -0500

    Create function remove and preserve data in localStorage

--- index.js
+++ index.js
@@ -1,38 +1,75 @@
-/* Create a collection of books */
-const books = [
-    {
-        tittle: 'Book 1',
-        author: 'Author 1',
-    },
-    {
-        tittle: 'Book 2',
-        author: 'Author 2',
-    },
-    {
-        tittle: 'Book 3',
-        author: 'Author 3',
-    },
-    {
-        tittle: 'Book 4',
-        author: 'Author 4',
-    },
-    {
-        tittle: 'Book 5',
-        author: 'Author 5',
-    }
-];
 
-/* Display all books save in the collection */
-const listbooks = document.querySelector('#list-books');
-const container = document.createElement('table');
-container.classList.add('table-item');
-books.forEach((book) => {
-    const container2 = document.createElement('tr');
-    container2.innerHTML= `
-        <td>"${book.tittle}"</td>
-        <td> by ${book.author}</td>
-        <td><button type="button" class="btn-remove">Remove</button></td>  
-    `;
-    container.appendChild(container2);
-    listbooks.appendChild(container);
-});
+const addBook = () => {
+    let books;
+    let bookTitle = document.querySelector('#title').value;
+    let booKAuthor = document.querySelector('#author').value;
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+    const bookObj = {
+      title: bookTitle,
+      author: booKAuthor
+    }
+    books.push(bookObj);
+  
+    localStorage.setItem('books', JSON.stringify(books));  
+  
+    return;
+}
+  
+const displayBook = () => {
+    const newBook = document.querySelector('.table-item');
+  
+    while (newBook.hasChildNodes()) {
+      newBook.removeChild(newBook.lastChild);
+    }
+  
+  
+    const books = JSON.parse(localStorage.getItem('books'));
+  
+  
+    books.forEach((book, index) => {
+      const newRow = document.createElement('tr');
+      newRow.innerHTML = `
+              <td>"${book.title}"</td>
+              <td> by ${book.author}</td>
+              <td><button type="button" class="btn-remove" id=${index}>Remove</button></td>  
+          `;
+      newBook.appendChild(newRow);  
+    })  
+}
+  
+  document.querySelector('.btn-submit').addEventListener('click', (e) => {
+    e.preventDefault();
+    addBook();
+    displayBook();
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
+  
+  });  
+  
+  const deleteBook = (elem) => {
+    const books = JSON.parse(localStorage.getItem('books'));
+    if (books.length === 1) {
+      books.pop();
+    } else {
+      books.splice(parseInt(elem.id), 1);
+    }
+  
+    localStorage.setItem('books', JSON.stringify(books));  
+  }
+  
+  const removeBook = (elem) => {
+  
+    elem.parentNode.parentNode.parentNode.removeChild(elem.parentNode.parentNode);
+  
+  }
+  
+  document.querySelector('.table-item').addEventListener('click', (e) => {
+    e.preventDefault();
+    removeBook(e.target);
+    deleteBook(e.target);  
+  });
+  
